d521d731693171ca5665be9cb2eca98f483e3b25
Jędrzej Jamnicki <jedrzej.jamnicki@gmail.com>
2021-09-27 14:37:32 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import io
+import os
+import time
+import pstats
+import cProfile
+from pathlib import Path
+from datetime import datetime
+
+
+def log_profile(log_folder="profile_logs"):
+    def wrap(func):
+        def inner(*args, **kwargs):
+            curr_time_hr = datetime.now()
+            curr_time_unixs = time.time()
+
+            pr = cProfile.Profile()
+
+            pr.enable()
+            retval = func(*args, **kwargs)
+            pr.disable()
+
+            stream = io.StringIO()
+            ps = pstats.Stats(pr, stream=stream).sort_stats('cumulative')
+            ps.print_stats()
+
+            report_content = stream.getvalue()
+
+            Path(log_folder).mkdir(parents=True, exist_ok=True)
+
+            script_path = func.__globals__['__file__']
+            log_title = f'{script_path}: {func.__name__} at {curr_time_hr}'
+            log_heading = f"{'='*10} {log_title} {'='*10}\n\n"
+            log_filename = f"log_{str(curr_time_unixs).replace('.', '_')}.txt"
+            log_path = os.path.join(log_folder, log_filename)
+
+            with open(log_path, 'w') as log_f:
+                log_f.writelines([log_heading, report_content])
+
+            return retval
+        return inner
+    return wrap
